65b73632882c694b5ac4b4dc90d579e4a2be4075
Shaun Oosthuizen <shaun.oosthuizen.wp@gmail.com>
2024-08-11 19:14:06 +0200

    Intro to React Refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,19 @@
+import React, { useState, useRef } from 'react';
+
 export default function Player() {
+  const playerName = useRef();
+  const [currentPlayerName, setCurrentPlayerName] = useState('unknown entity');
+  
+  function handleSetName() {
+    setCurrentPlayerName(playerName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {currentPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleSetName}>Set Name</button>
       </p>
     </section>
   );
