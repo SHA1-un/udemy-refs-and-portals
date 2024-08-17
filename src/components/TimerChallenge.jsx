5cae582db26a80303d23f3761add14cd128ad3b7
Shaun Oosthuizen <shaun.oosthuizen.wp@gmail.com>
2024-08-17 16:18:44 +0200

    Added modal that shows when your time runs out

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,36 +1,41 @@
 import { useRef, useState } from "react";
+import ResultModel from "./ResultModel";
 
-export default function TimerChallenge({title, targetTimeSeconds}) {
+export default function TimerChallenge({ title, targetTimeSeconds }) {
   const targetTimeMs = targetTimeSeconds * 1000;
   const timer = useRef();
+  const dialog = useRef();
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
-  
+
   function handleStart() {
     setTimerStarted(true);
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTimeMs);
   }
 
   function handleStop() {
     clearTimeout(timer.current);
   }
-  return <section className="challenge">
-    <h2>{title}</h2>
-    {timerExpired && <p>You lost!</p>}
-    <p className="challenge-time">
+  return <>
+    <ResultModel ref={dialog} targetTime={targetTimeSeconds}/>
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
         {targetTimeSeconds} second{targetTimeSeconds > 1 ? 's' : ''}
-    </p>
-    <p>
-      <button onClick={timerStarted ? handleStop : handleStart}>
-        {timerStarted ? 'Stop' : 'Start'} Challenge
-      </button>
-    </p>
-    <p className={timerStarted ? 'active' : undefined}>
-      {timerStarted ? 'Timer is running...' : 'Timer inactive'}
-    </p>
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? 'Stop' : 'Start'} Challenge
+        </button>
+      </p>
+      <p className={timerStarted ? 'active' : undefined}>
+        {timerStarted ? 'Timer is running...' : 'Timer inactive'}
+      </p>
 
-  </section>
+    </section>
+  </>
 }
